ec8d1a90881948d06f1efe7a9f5f1549479610b8
승찬님 <chan_0524@naver.com>
2025-05-28 14:23:01 +0900

    Fix: supabase import conflict

--- src/scores.js
+++ src/scores.js
@@ -1,4 +1,4 @@
-import { supabase } from '../supabaseClient'
+import supabase from "./supabaseClient";
 
 export const submitScore = async (nickname, score) => {
   const { data, error } = await supabase.from('scores').insert([
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-import { supabase } from "./supabaseClient";
+import { createClient } from '@supabase/supabase-js';
 
-
-const supabaseUrl = 'https://ppbwflcdpznxibrbngac.supabase.co'
+const supabaseUrl = 'https://ppbwflcdpznxibrbngac.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYndmbGNkcHpueGlicmJuZ2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MDYwMzYsImV4cCI6MjA2Mzk4MjAzNn0.8YW6Xj7kySX6Knc1-5_poIFePVORwhA0pEBdl-DO9lo'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
