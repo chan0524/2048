f6f6cc99a9ab270ffd59fa88a798de97bb0fd46c
승찬님 <chan_0524@naver.com>
2025-05-28 14:38:40 +0900

    fix: default export supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ppbwflcdpznxibrbngac.supabase.co';
+const supabaseUrl = 'https://ppbwflcdpznxibrbngac.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYndmbGNkcHpueGlicmJuZ2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MDYwMzYsImV4cCI6MjA2Mzk4MjAzNn0.8YW6Xj7kySX6Knc1-5_poIFePVORwhA0pEBdl-DO9lo'
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export default supabase
